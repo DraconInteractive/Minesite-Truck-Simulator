d97f71fe81359a69fb71bb3634c95d5c5c1abf28
Peter Carey <petermarkcarey@gmail.com>
2026-04-15 20:04:46 +0800

    feat: structures for ids and events, init collections

--- TruckSim/TruckSim.cpp
+++ TruckSim/TruckSim.cpp
@@ -1,11 +1,50 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 #include "Core/console.h"
+#include "Entities/Mobile/Truck.h"
+#include "Entities/Stationary/Dump.h"
+#include "Entities/Stationary/Shovel.h"
 
 struct AppState
 {
     bool running;
+};
+
+// Create structs for entity id classification - stops me from assigning shovel ID to truck ID function
+constexpr int INVALID_ENTITY_ID = -1;
+
+struct TruckId
+{
+    int value = INVALID_ENTITY_ID;
+};
+
+struct ShovelId
+{
+    int value = INVALID_ENTITY_ID;
+};
+
+struct DumpId
+{
+    int value = INVALID_ENTITY_ID;
+};
+
+enum class EventType : uint8_t
+{
+    TruckArriveShovel,
+    TruckFinishLoading,
+    TruckArriveDump,
+    TruckFinishDumping
+};
+
+struct Event
+{
+    double time;
+    TruckId truck;
+    ShovelId shovel;
+    DumpId dump;
+    EventType type;
 };
 
 int main(int argc, char* argv[])
@@ -16,6 +55,25 @@ int main(int argc, char* argv[])
     console::enableAnsi();
     // console::enableRawInput(); // For later - better navigation etc
 
+    std::vector<Truck> trucks;
+    std::vector<Shovel> shovels;
+    std::vector<Dump> dumps;
+
+    // TODO move the above structs to their own area, and guide this init from a config file
+
+    trucks.reserve(3);
+    for (int i = 0; i < 3; i++)
+    {
+        // Use emplace to construct in-place instead of construct->copy
+        trucks.emplace_back(i, 60, 100, 0);
+    }
+
+    shovels.reserve(1);
+    shovels = { Shovel(0, 10) };
+
+    dumps.reserve(1);
+    dumps = { Dump(0)};
+    
     while (state.running)
     {
         std::string inputLine;
